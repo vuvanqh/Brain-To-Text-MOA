55e1a21b9f472bdfb97d0a8a61f94691294d61ac
BanhBaoz <q.vuvan05@gmail.com>
2025-12-16 15:49:37 +0100

    aug

--- model_training/dataset.py
+++ model_training/dataset.py
@@ -5,7 +5,7 @@ import h5py
 import numpy as np
 from torch.nn.utils.rnn import pad_sequence
 import math 
-from model_training.data_augmentations import temporal_mask
+from data_augmentations import temporal_mask
 class BrainToTextDataset(Dataset):
     '''
     Dataset for brain-to-text data
@@ -137,7 +137,7 @@ class BrainToTextDataset(Dataset):
                         input_features = torch.from_numpy(input_features) # neural data
                         #/TEAM COLOMBIA
 
-                        #for preliminary eda uncomment the line below and comet out the segment of team colombia above
+                        #KASIA: for preliminary eda uncomment the line below and comet out the segment of team colombia above
                         #input_features = torch.from_numpy(g['input_features'][:]) # neural data
                         if self.feature_subset:
                             input_features = input_features[:,self.feature_subset]
