bb0724dd3e051350c0a93698679ecc1ea7e8e986
BanhBaoz <q.vuvan05@gmail.com>
2025-12-31 20:25:52 +0100

    ok

--- analyses/trial.py
+++ analyses/trial.py
@@ -27,10 +27,9 @@ def run_command(command, prefix, color=""):
 
 # Define your commands
 cmd1 = (
-    "cd /Brain-To-Text-MOA && "
     "alias conda='~/miniconda3/bin/conda' && "
     "source ~/miniconda3/bin/activate && "
-    "(conda activate b2txt25_lm || bash setup_lm.sh) && "
+    "bash setup_lm.sh && "
     "conda activate b2txt25_lm && "
     "python language_model/language-model-standalone.py "
     "--lm_path language_model/pretrained_language_models/openwebtext_1gram_lm_sil "
@@ -41,19 +40,20 @@ cmd1 = (
     "--redis_ip localhost "
     "--gpu_number 0"
 )
+
 cmd2 = (
-    "sleep 10 && "
-    "cd /Brain-To-Text-MOA && "
+    "sleep 900 && "
     "alias conda='~/miniconda3/bin/conda' && "
     "source ~/miniconda3/bin/activate && "
-    "(conda activate b2txt25 || bash setup.sh) && "
+    "bash setup.sh && "
     "conda activate b2txt25 && "
     "python model_training/evaluate_model.py "
     "--gpu_number 0 "
-    "--model_path /transformer_v1 "
+    "--model_path /kaggle/input/trained_models/transformer_v1 "
+    "--data_dir /kaggle/input/trained_models/t15_copyTask_neuralData/hdf5_data_final"
     "--eval_type val"
+    "--csv_path data/t15_copyTaskData_description.csv"
 )
-
 
 # cmd1 = "alias conda='~/miniconda3/bin/conda' && source ~/miniconda3/bin/activate && bash setup_lm.sh && conda activate b2txt25_lm && python language_model/language-model-standalone.py --lm_path language_model/pretrained_language_models/openwebtext_1gram_lm_sil --do_opt --nbest 100 --acoustic_scale 0.325 --blank_penalty 90 --alpha 0.55 --redis_ip localhost --gpu_number 0"
 # cmd2 = "echo 'Sleeping for 15 minutes...' && sleep 900 && alias conda='~/miniconda3/bin/conda' && source ~/miniconda3/bin/activate && bash setup.sh && conda activate b2txt25 && python model_training/evaluate_model.py --gpu_number 1 --model_path /kaggle/input/brain-to-text-25/t15_pretrained_rnn_baseline/t15_pretrained_rnn_baseline/ --data_dir /kaggle/input/brain-to-text-25/t15_copyTask_neuralData/hdf5_data_final --csv_path ./data/t15_copyTaskData_description.csv --eval_type val"
